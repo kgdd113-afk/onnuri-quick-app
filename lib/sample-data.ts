a70564af57613508d78c5e7054187e40d11b8697
병수 공 <kgdd113@gmail.com>
2026-03-12 15:33:38 +0900

    sampleItems export 이름 수정

--- lib/sample-data.ts
+++ lib/sample-data.ts
@@ -1,18 +1,10 @@
-export const sampleData = [
+export const sampleItems = [
   {
     date: "2024-01-01",
     from: "서울 중구",
     to: "수원시청",
     vehicle: "오토바이",
     price: 35000,
-    note: "샘플 데이터"
+    note: "샘플 데이터",
   },
-  {
-    date: "2024-01-02",
-    from: "향남",
-    to: "의왕 포일동",
-    vehicle: "1톤",
-    price: 55000,
-    note: "샘플 데이터"
-  }
 ];
